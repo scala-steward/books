1743c255175a50eb4d6ac4c9709e88e687e2005d
Nader Hajlaoui <hajlaoui.nader@gmail.com>
2020-09-07 10:47:31 +0200

    add description + categories

--- modules/core/src/main/scala/xyz/funnycoding/domain/volume.scala
+++ modules/core/src/main/scala/xyz/funnycoding/domain/volume.scala
@@ -7,12 +7,21 @@ import scala.util.control.NoStackTrace
 object volume {
   @newtype case class VolumeId(value: NonEmptyString)
 
-  @newtype case class VolumeIdparam(value: NonEmptyString)
-
+  @newtype case class Description(value: NonEmptyString)
+  @newtype case class Category(value: NonEmptyString)
   @newtype case class Title(value: NonEmptyString)
   @newtype case class Subtitle(value: NonEmptyString)
   @newtype case class Author(value: NonEmptyString)
-  case class VolumeInfo(title: Title, subtitle: Subtitle, authors: List[Author])
+  @newtype case class Publisher(value: NonEmptyString)
+
+  case class VolumeInfo(
+      title: Title,
+      subtitle: Subtitle,
+      authors: List[Author],
+      description: Description,
+      categories: List[Category]
+  )
+
   case class Volume(id: VolumeId, volumeInfo: VolumeInfo)
 
   case class VolumeError(cause: String) extends NoStackTrace
