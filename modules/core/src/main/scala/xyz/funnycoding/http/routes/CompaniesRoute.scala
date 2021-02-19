d2c6e381ec26a4af193a8c061875084e2e8f9244
Nader Hajlaoui <hajlaoui.nader@gmail.com>
2021-02-19 15:35:58 +0100

    optimize imports

--- modules/core/src/main/scala/xyz/funnycoding/http/routes/CompaniesRoute.scala
+++ modules/core/src/main/scala/xyz/funnycoding/http/routes/CompaniesRoute.scala
@@ -1,23 +1,22 @@
 package xyz.funnycoding.http.routes
 
 import cats._
-import cats.implicits._
 import org.http4s._
 import org.http4s.circe.JsonDecoder
 import org.http4s.dsl.Http4sDsl
 import org.http4s.server.Router
 import xyz.funnycoding.algebras.Companies
-import xyz.funnycoding.domain.data.Company
 import xyz.funnycoding.effects._
 import xyz.funnycoding.http.json._
 
-final class CompaniesRoute[F[_]: Defer: MonadThrow: JsonDecoder](contacts: Companies[F]) extends Http4sDsl[F] {
+final class CompaniesRoute[F[_]: Defer: MonadThrow: JsonDecoder](companies: Companies[F]) extends Http4sDsl[F] {
   private[routes] val prefixPath = "/companies"
 
-  private val httpRoutes = HttpRoutes.of[F] { case GET -> Root =>
-    Ok(
-      contacts.findAll
-    )
+  private val httpRoutes = HttpRoutes.of[F] {
+    case GET -> Root =>
+      Ok(
+        companies.findAll
+      )
   }
 
   val routes: HttpRoutes[F] = Router(
